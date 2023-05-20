d750e7648e00cbfebbac3328273a92188b598457
DavesApps <DavesApps@users.noreply.github.com>
2023-05-20 14:48:05 -0400

    Updated

---
+++ Chapter 30 - Multithreading/ThreadAffinity.cs
@@ -0,0 +1,14 @@
+BeginThreadAffinity, EndThreadAffinity
+
+
+[SecurityPermission(SecurityAction.Demand, Flags=SecurityPermissionFlag.ControlThread)]
+ public class WrapperClassForThreadAffinity
+  { 
+    [SecurityPermission(SecurityAction.Demand, Flags=SecurityPermissionFlag.ControlThread)]
+     public void PerformMyTask()
+      { 
+        // Code that does not have thread affinity goes here.
+         Thread.BeginThreadAffinity(); // Code that has a thread affinity goes here. 
+         Thread.EndThreadAffinity(); // And More code that doesn’t have a thread affinity. 
+      }
+  }
